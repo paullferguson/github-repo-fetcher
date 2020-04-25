3b9f25a32edb1acc86c3162aeecd561507f94b07
Paul Ferguson <paullferguson@gmail.com>
2020-04-25 10:59:00 -0700

    Styling the lists

--- client/src/components/RepoItem.jsx
+++ client/src/components/RepoItem.jsx
@@ -10,21 +10,23 @@ class RepoItem extends React.Component {
   render() {
     const { name, full_name, owner, html_url, description, url, created_at, updated_at, size, stargazers_count, watchers_count, forks_count, open_issues_count } = this.props.info;
     return(
-      <li>
+      <li className="repo-item">
           <a href={html_url} target="_blank">
-            <img src={owner.avatar_url} alt="{owner.login}" height="46" width="45"/>
-            <div>
-              <h3>{name}</h3>
-              <h5>by {owner.login}</h5>
-              <small>Created {created_at}</small>
+            <div className="repo-item__header">
+              <img src={owner.avatar_url} alt="{owner.login}" height="46" width="45"/>
+              <div>
+                <h3>{name}</h3>
+                <h5>by {owner.login}</h5>
+                <small>Created {created_at}</small>
+              </div>
             </div>
             <p>{description}</p>
-            <ul>
-              <li>size: {size}</li>
-              <li>stargazers_count: {stargazers_count}</li>
-              <li>watchers_count: {watchers_count}</li>
-              <li>forks_count: {forks_count}</li>
-              <li>open_issues_count: {open_issues_count}</li>
+            <ul className="repo-item__details">
+              <li>size: <strong>{size}</strong></li>
+              <li>stars: <strong>{stargazers_count}</strong></li>
+              <li>watchers: <strong>{watchers_count}</strong></li>
+              <li>forks: <strong>{forks_count}</strong></li>
+              <li>open issues: <strong>{open_issues_count}</strong></li>
             </ul>
           </a>
       </li>
